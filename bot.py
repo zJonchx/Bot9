8d117e95d26d822903b382bc08bc4ed710ab05d2
zJonchx <jonathaneliaschavez123@gmail.com>
2025-12-12 17:56:32 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def ejecutar_ataque(comando: str, ctx, ip: str, port: int, tiempo: int):
 @bot.command(name='attack', help='!attack {method} {ip} {port} {time}')
 async def attack(ctx, metodo: str = None, ip: str = None, port: str = None, tiempo: str = None):
     if metodo is None or ip is None or port is None or tiempo is None:
-        await ctx.send("!attack {method} {ip} {port} {time}")
+        await ctx.send("!attack {method} {ip} {port} {time} 11")
         return
 
     # Verificar que no haya nulls
